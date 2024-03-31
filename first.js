62396f28e2c8015dd4061dc4d451f668807f0993
muxbe <159569051+muxbe@users.noreply.github.com>
2024-03-31 18:59:32 +0200

    js1

---
+++ first.js
@@ -0,0 +1,22 @@
+let myname = " abeli ";
+let myfamily = " gulbani ";
+const yearofbirth = 1997;
+let currentYear = 2024;
+let myHoby = " fishing ";
+let nationality = " georgian";
+const learnstart = 2024;
+let learnend = 2070;
+let learntime = learnend - learnstart;
+let age = 27;
+
+console.log("my name is ", myname);
+console.log("last name ", myfamily);
+console.log("my age is ", currentYear - yearofbirth);
+console.log("my hobby is ", myHoby);
+console.log("nationality ", nationality);
+console.log("learn time ", learntime, " year");
+
+nationality = "georgia ";
+
+let fullinfo = myname + myfamily + " from " + nationality + age + " year old";
+console.log(fullinfo);
