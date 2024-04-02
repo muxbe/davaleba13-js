ccb34cebd1408b4ee9b82dfd065e25764c0cb227
muxbe <159569051+muxbe@users.noreply.github.com>
2024-04-02 09:39:30 +0200

    addd

--- first.js
+++ first.js
@@ -8,6 +8,13 @@ const learnstart = 2024;
 let learnend = 2070;
 let learntime = learnend - learnstart;
 let age = 27;
+const big = 1.03e8;
+let plus = 2;
+let Plus = ++plus;
+let gam = 3;
+let Gam = ++gam * 7;
+let rva = 8;
+let cxra = 9;
 
 console.log("my name is ", myname);
 console.log("last name ", myfamily);
@@ -15,6 +22,10 @@ console.log("my age is ", currentYear - yearofbirth);
 console.log("my hobby is ", myHoby);
 console.log("nationality ", nationality);
 console.log("learn time ", learntime, " year");
+console.log(big);
+console.log(Plus);
+console.log(Gam);
+console.log(rva > cxra);
 
 nationality = "georgia ";
 
